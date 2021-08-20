d11d5bab563e67ab60dcdd7ce9f62f5deac0bba3
LayMui <mui_ume@hotmail.com>
2021-08-20 12:24:59 +0800

    Simple exercise on routing

---
+++ src/components/DetailComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>I am the detail component</p>
+        <button @click="goToMaster">Master</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'DetailComponent',
+        methods: {
+            goToMaster() {
+                this.$router.push('/');
+            }
+        }
+    }
+</script>
---
+++ src/components/MasterComponent.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+    <p>This is the master component</p>
+    <router-link :to='detailsPath' >Go to detail</router-link>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'MasterComponent',
+    data() {
+        return {
+            detailsPath: '/detail'
+        }
+    }
+}
+</script>
